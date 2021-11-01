b59951c51ec967375e4378c6ad083bce0c861d90
Kinder <nguyenductrong12a1@gmail.com>
2021-11-01 13:29:37 +0700

    Init

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name="Trong-Nguyen-Utils",
+    version="0.0.1",
+    description="Create for test",
+    install_requires=[
+        "sklearn",
+        "psutil"
+    ]
+)
